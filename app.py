e3374b6606cdb0caad8c3101f8e029e95e664619
faderosyad <faderosyad@gmail.com>
2019-03-06 17:43:46 +0700

    06/03/19 - Can do Multiple post for Key and Value

--- app.py
+++ app.py
@@ -5,22 +5,21 @@
 #Hal ini terjadi dikarenakan terdapat perbedaan cara penggunaan yang menyebabkan dibutuhkan requests pada function dan request pada route
 import requests, json
 from flask import Flask, render_template, request, json
-from flask.views import View
 from datetime import datetime
 
 def get_timestamp():
-    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
+    return datetime.now().strftime(("%d-%m-%Y %H:%M:%S"))
 
 KVStoreEndpoint = 'http://127.0.0.1:5000/api/v1/kvstore' #Endpoint URL untuk Genesis IVA bagian KVStore
 ServicesEndpoint = 'http://127.0.0.1:5000/api/v1/services' #Endpoint URL untuk Genesis IVA bagian Services
 headers = {'Content-type': 'application/json'} # Header yang dapat diterima oleh Genesis IVA
 
-#Function untuk menerima data dari Genesis IVA dengan method GET
+#Function untuk menerima data dari Genesis IVA dengan method GET (KeyValue)
 def gettingdata():
     terima = requests.get(KVStoreEndpoint, headers=headers)
     return terima
 
-#Function untuk mengirimkan data ke Genesis IVA dengan method POST
+#Function untuk mengirimkan data ke Genesis IVA dengan method POST (KeyValue)
 def postingdata(key, value):
     data = {'key': key, 'value': value}
     kirim = requests.post(KVStoreEndpoint, data=json.dumps(data), headers=headers)
@@ -40,12 +39,15 @@ def ivaconfig():
 @app.route('/keyvalue', methods = ['POST', 'GET'])
 def keyvalue():
     if request.method == 'POST': 
-    #Disini perlu diiterasikan untuk membentuk pengiriman data secara iterasi dalam sekali operasi
-        key = request.form.get('key')
-        value = request.form['value']
-        kirimin = postingdata(key, value)
-        timestamp = get_timestamp()
-        return render_template('home2.html', key=key, value=value, timestamp=timestamp)
+    #iterasi untuk mengirimkan banyak data key dan value secara bersamaan
+            i = 0
+            while i < 100: #Pembatasan pengiriman hanya untuk 100 key dan value
+                key = request.form.get('key'+ str(i))
+                value = request.form['value'+ str(i)]
+                kirimin = postingdata(key, value)
+                i += 1
+            timestamp = get_timestamp()
+            return render_template('home2.html', key=key, value=value, timestamp=timestamp) #masih terdapat masalah dalam penampilan ke table, akan dicoba untuk melakukan GET ke table
 
     return render_template('home2.html')
 
