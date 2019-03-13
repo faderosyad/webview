9fa77d431e8b341a796b8df21009c08acfeb1300
faderosyad <faderosyad@gmail.com>
2019-03-13 19:55:50 +0700

    13/03/19 - Can GET and POST but bad template display

--- app.py
+++ app.py
@@ -25,10 +25,10 @@ def postingdatas(service):
     kirims = requests.post(ServicesEndpoint, data=json.dumps(data), headers=headers)
     return kirims
 
-#Function untuk menerima data dari Genesis IVA dengan method GET (KeyValue)
-def gettingdata():
-    terima = requests.get(KVStoreEndpoint, headers=headers)
-    return terima
+##Function untuk menerima data dari Genesis IVA dengan method GET (KeyValue)
+#def gettingdata():
+#    terima = requests.get(KVStoreEndpoint, headers=headers)
+#    return terima
 
 #Function untuk mengirimkan data ke Genesis IVA dengan method POST (KeyValue)
 def postingdata(key, value):
@@ -54,18 +54,34 @@ def ivaconfig():
     
 @app.route('/keyvalue', methods = ['POST', 'GET'])
 def keyvalue():
+    #if request.method == 'GET':
+    terima = requests.get(KVStoreEndpoint, headers=headers) # menghubungkan ke genesis API dan mengambil datanya
+    batas = json.loads(terima.text) #mengeluarkan hasil dari get tersebut berupa json
+    j = 0
+    panjang = len(batas)#menentukan panjang dari keseluruhan array json yang didapat
+    listkey = [] #array untuk menampung key
+    listvalue = [] #array untuk menampung value
+    while j <= (panjang-1):
+        tampil = json.loads(terima.text)[j] #memunculkan data ke i pada array yang didapat 
+        keygen = tampil['key']
+        valuegen = tampil['value']
+        listkey.append(keygen)
+        listvalue.append(valuegen)
+        j+=1
+
+        #return render_template('home2.html')
     if request.method == 'POST': 
     #iterasi untuk mengirimkan banyak data key dan value secara bersamaan
             i = 0
-            while i < 100: #Pembatasan pengiriman hanya untuk 100 key dan value
+            while i < 1: #Pembatasan pengiriman hanya untuk 100 key dan value
                 key = request.form.get('key'+ str(i))
                 value = request.form['value'+ str(i)]
                 kirimin = postingdata(key, value)
                 i += 1
-            timestamp = get_timestamp()
-            return render_template('home2.html', key=key, value=value, timestamp=timestamp) #masih terdapat masalah dalam penampilan ke table, akan dicoba untuk melakukan GET ke table
+            #timestamp = get_timestamp()
+            return render_template('home2.html', listkey=listkey, listvalue=listvalue)
 
-    return render_template('home2.html')
+    return render_template('home2.html', listkey=listkey, listvalue=listvalue)
 
 if __name__ == '__main__':
     app.run(debug = True, port= 2000)
